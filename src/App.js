04e6fdce6c063bbae122b8738627960c9067d148
Richard Hill III <rhillz718@gmail.com>
2018-06-07 01:07:36 -0400

    added navigation, still working on it

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import MainScreen from './Components/MainScreen';
 import ExpenseReport from './Components/ExpenseReport';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 
 class App extends Component {
-  render() {
+    //      state={
+    //     drawerIsOpen: false,
+    //     expanded: false
+
+    // }
+
+    // toggleDrawer = () => {
+    //     if(this.state.drawerIsOpen){
+    //         this.setState({drawerIsOpen: false})
+    //     } else {
+    //         this.setState({drawerIsOpen: true})
+    //     }
+    // }
+
+
+
+  render(props) {
+    console.log(props)
     return (
       <div className="App">
       <MuiThemeProvider>
-        <MainScreen/>
+        <Router>
+          <div>
+            <Route path="/" exact strict component={MainScreen}/>
+            <Route path="/expense" component={ExpenseReport}/>
+          </div>
+        </Router>
       </MuiThemeProvider>
 
       </div>
     );
   }
 }
+
+
+
 
 export default App;
--- src/Components/Drawer.js
+++ src/Components/Drawer.js
@@ -3,16 +3,23 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
 import Clear from 'material-ui/svg-icons/content/clear'
-
+import RaisedButton from 'material-ui/RaisedButton';
+import {Link} from 'react-router-dom';
 
 
 const MenuDrawer = props => {
-   console.log(props)
+
         return(
             <Drawer open = {props.open} >
                 
                 <Clear onClick={props.toggle} />
-                <MenuItem>Expense Report</MenuItem>
+    
+                <Link to="/expense">
+                    <RaisedButton fullWidth={true}>
+                        Expense Report
+                    </RaisedButton>
+                </Link>
+            
                 <Divider/>
                 <MenuItem>Customer Log</MenuItem>
                 <Divider/>
